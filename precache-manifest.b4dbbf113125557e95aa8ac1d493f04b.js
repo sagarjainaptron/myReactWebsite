cd67391e0a340a4b6a9dafee0ed9e9ccaddddc60
sagar jain <jainsagar8888@gmail.com>
2020-08-04 16:39:54 +0530

    Updates

---
+++ precache-manifest.b4dbbf113125557e95aa8ac1d493f04b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a87d399095aa512f55b7eef9b5a0762",
+    "url": "/myReactWebsite/index.html"
+  },
+  {
+    "revision": "6aa4597f9a9c4d5c18fb",
+    "url": "/myReactWebsite/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "1a18bd595eba3d2dd947",
+    "url": "/myReactWebsite/static/css/main.d92a82b9.chunk.css"
+  },
+  {
+    "revision": "6aa4597f9a9c4d5c18fb",
+    "url": "/myReactWebsite/static/js/2.033ce4c4.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/myReactWebsite/static/js/2.033ce4c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a18bd595eba3d2dd947",
+    "url": "/myReactWebsite/static/js/main.8834c2b0.chunk.js"
+  },
+  {
+    "revision": "91c3e5ff14baa8881efa",
+    "url": "/myReactWebsite/static/js/runtime-main.c438d3f4.js"
+  }
+]);
